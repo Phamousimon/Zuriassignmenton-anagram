50cd5139594ab4feb78175730b4421918907c0a5
Phamous <42639935+Phamousimon@users.noreply.github.com>
2022-05-26 16:25:08 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+def find_anagram(word, anagram):
+    # [assignment] Add your code here
+    return sorted(word) == sorted(anagram)
+
+print(find_anagram("night", "thing"))
+    
